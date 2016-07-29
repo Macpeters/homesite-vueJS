cac77cd574241c8298a5960067bf3d2eef612e9f
Macpeters <peters.maryanne@gmail.com>
2016-07-29 18:51:29 -0400

    redid this thing from scratch

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
-'use strict';
-
-/* App Module */
-
-angular.module('website', ['websiteFilters', 'gamecatServices']).
-	config(['$routeProvider', function($routeProvider){
-		$routeProvider.
-			when('/dev', {templateUrl: 'partials/home.html'}).
-			when('/programming', {templateUrl: 'partials/dev.html', controller: ProgrammingCtrl}).
-      when('/ravemau5_iframe', {templateUrl: 'partials/ravemau5_iframe.html', controller: ProgrammingCtrl}).
-			when('/artwork', {templateUrl: 'partials/art.html', controller: ArtCtrl}).
-			otherwise({redirectTo: '/dev'});
-	}]);
+$(function() {
+		$('.pop').on('click', function(e) {
+			e.preventDefault();
+			var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+			if(width > 450){
+				$('.imagepreview').attr('src', $(this).find('img').attr('src'));
+				$('.imagepreview').attr('alt', $(this).find('img').attr('alt'));
+				$('#imagemodal').modal('show');
+			}
+		});
+});
--- js/services.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* Services */
-angular.module('gamecatServices', ['ngResource']).
-	factory('Game', function($resource){
-		return $resource('json/:gameId.json', {}, {
-			query: {method:'GET', params:{gameId:'games'}, isArray:true}
-		});
-});

839db43739154fbb47a5d6155449ab4234d4a2d8
Macpeters <peters.maryanne@gmail.com>
2017-10-28 15:54:23 -0400

    new shop page

--- js/app.js
+++ js/app.js
@@ -5,7 +5,13 @@ $(function() {
 			if(width > 450){
 				$('.imagepreview').attr('src', $(this).find('img').attr('src'));
 				$('.imagepreview').attr('alt', $(this).find('img').attr('alt'));
+
+				$('.imagepreview-title').text($(this).find('img').attr('title'));
+				$('.imagepreview-description').text($(this).find('img').attr('data-description'));
+
 				$('#imagemodal').modal('show');
 			}
 		});
+
+
 });
